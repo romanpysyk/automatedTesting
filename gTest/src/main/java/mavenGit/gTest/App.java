cf5c538bc72ab523f3635ded3d70cc43f98cae1d
Roman <Roman@Roman-PC>
2017-09-23 17:54:41 +0300

    please work

---
+++ gTest/src/main/java/mavenGit/gTest/App.java
@@ -0,0 +1,29 @@
+package mavenGit.gTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class App 
+{
+protected WebDriver chrome;
+    
+    @Test
+    public void gitPush() throws InterruptedException {
+    	System.setProperty("webdriver.chrome.driver", "C:/Users/SeleniumDrivers/chromedriver.exe");
+    	
+    	chrome = new ChromeDriver();
+    	String eTitle = "Meet Guru99 - Free Training Tutorials & Video for IT Courses";
+    	String aTitle = "";
+    	chrome.get("http://www.guru99.com/");
+    	chrome.manage().window().maximize();
+    	aTitle = chrome.getTitle();
+    	
+    	if(aTitle.contentEquals(eTitle)){
+    		System.out.println("TEST PASSED ! ! !");
+    	} else {
+    		System.out.println("TEST FAILED :( ");
+    	}
+    	chrome.close();
+    }
+}
